af91cbf5bb72b46fd712b7c44d3f5ffab75fc4f1
sudhaarora <a.1994sudha@gmail.com>
2023-02-17 13:45:02 +0530

    add styledButton, add style dynamically with help of props, add image

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
 import styles from "./styles";
+import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
   return (
     <View style={styles.carContainer}>
       <ImageBackground
-        style={styles.image}  
+        style={styles.image}
         source={require("../../assets/images/ModelX.jpeg")}
       />
+
       <View style={styles.titles}>
         <Text style={styles.title}>Modal S</Text>
         <Text style={styles.subTitle}>Starting at $69,420</Text>
       </View>
+
+      <StyledButton 
+        type="primary" 
+        content={"Custom Order"}
+        onPress={() => console.warn("Custom Order was Pressed!")} />
+      
+      <StyledButton 
+        type="secondary" 
+        content={"Existing Inventory"}
+        onPress={() => console.warn("Existing Inventory was Pressed!")} />
+
     </View>
   );
 };
